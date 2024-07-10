fa4dd6bf9d5d7138ca85e4ff7a067b73b726a0fd
joshkim2441 <joshkim2441@gmail.com>
2024-07-10 23:51:38 +0300

    Corrections

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,5 +2,5 @@
 SELECT band_name AS band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC;limitations under the License.
+ORDER BY lifespan DESC;
 
