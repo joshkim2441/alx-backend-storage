a340d3c60ed48462468ecf5c0d59eccc14bfed14
joshkim2441 <joshkim2441@gmail.com>
2024-07-15 13:34:41 -0700

    Task 14

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -5,23 +5,13 @@
 def top_students(mongo_collection):
     """ returns all students sorted by average score: """
 
-    pipeline = mongo_collection.aggregate(
-        [
+    pipeline = mongo_collection.aggregate([
             {
                 "$project": {
-                    "_id": 1,
-                    "name": 1,
-                    "averageScore": {
-                        "avg": {
-                            "$avg": "$topics.score",
-                        },
-                    },
-                    'topics': 1,
-                },
+                    "name": "$name",
+                    "averageScore": {"$avg": "$topics.score"}
+                }
             },
-            {
-                "$sort": {"averageScore": -1},
-            },
-        ]
-    )
+            {"$sort": {"averageScore": -1}}
+    ])
     return pipeline
